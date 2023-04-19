892ea0dd81f68205b97e9de46fa1f56cb6337334
Juliana Villegas <jumavipe@hotmail.com>
2023-04-19 08:21:39 -0500

    changed titles for products (#2)

--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 const products = [
     {
         imgUrl: "https://guesseu.scene7.com/is/image/GuessEU/M63H24W7JF0-L302-ALTGHOST?wid=1500&fmt=jpeg&qlt=80&op_sharpen=0&op_usm=1.0,1.0,5,0&iccEmbed=0",
-        name: "CHECK PRINT SHIRT",
+        name: "addfdg PRINT SHIRT",
         price: 110,
     },
     {
@@ -11,12 +11,12 @@ const products = [
     },
     {
         imgUrl: "https://guesseu.scene7.com/is/image/GuessEU/HWVG6216060-TAN?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
-        name: "CATE RIGID BAG",
+        name: "CATE dghfghh BAG",
         price: 94.5
     },
     {
         imgUrl: "http://guesseu.scene7.com/is/image/GuessEU/WC0001FMSWC-G5?wid=520&fmt=jpeg&qlt=80&op_sharpen=0&op_usm=1.0,1.0,5,0&iccEmbed=0",
-        name: "fdsg CONNECT WATCH",
+        name: "fdsg hfghghf CONNECT WATCH",
         price: 438.9
     },
     {
@@ -30,7 +30,7 @@ const products = [
     },
     {
         imgUrl: "https://guesseu.scene7.com/is/image/GuessEU/FLGLO4FAL12-BEIBR?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
-        name: "GLORIA HIGH LOGO SNEAKER",
+        name: "GLORIA HIGH LOGO hfgghfhgf",
         price: 91
     },
     {
